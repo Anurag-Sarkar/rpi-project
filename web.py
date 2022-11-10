5262568f2b54e7477bcad4b1dc31e54722d40004
unknown <sarkaranurag104@gmail.com>
2022-11-09 21:02:39 -0800

    h

--- web.py
+++ web.py
@@ -22,6 +22,7 @@ global identity
 #---------------LIBRAREIS--------------------
 def print_f(pin):
     print("Button Pressed........................")
+    print(time.time())
 def enroll_finger(location):
     """Take a 2 finger images and template it, then store in 'location'"""
     for fingerimg in range(1, 3):
@@ -291,7 +292,7 @@ def message(data):
         print("Cant add fingerprint----------------------------")   
         socket.emit("fail")
 
-GPIO.add_event_detect(2, GPIO.FALLING, callback=print_f, bouncetime=10)
+GPIO.add_event_detect(26, GPIO.FALLING, callback=print_f, bouncetime=10)
 
 socket.run(app,host="192.168.29.7",port="80",debug=True)
 
