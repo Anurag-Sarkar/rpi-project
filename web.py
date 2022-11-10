864fa76a50ff8b7aeb4a141e444fc25cb1ec69da
unknown <sarkaranurag104@gmail.com>
2022-11-09 23:36:00 -0800

    h

--- web.py
+++ web.py
@@ -208,11 +208,10 @@ def exit():
         time = x.strftime("%H:%M")    
         date = x.strftime("%d-%m-%Y")
         check = attendence.find_one({"name":n})
-        print(check)
-        # if check and :
-        #     attendence.find_one_and_update({"name":n},{ '$set': { "exit" : time}},return_document=ReturnDocument.AFTER)
-        # else:
-        #     print("user exited")
+        if check and check["date"] == date and check["exit"] == "-":
+            attendence.find_one_and_update({"name":n},{ '$set': { "exit" : time}},return_document=ReturnDocument.AFTER)
+        else:
+            print("user exited")
     else:
         print("FUCK YOU")
     return redirect('/')
