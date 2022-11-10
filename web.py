b831749fd093c88a766b78bca20a96ddedcf50de
unknown <sarkaranurag104@gmail.com>
2022-11-09 21:39:07 -0800

    h

--- web.py
+++ web.py
@@ -298,8 +298,7 @@ def message(data):
         print("Cant add fingerprint----------------------------")   
         socket.emit("fail")
 
-GPIO.wait_for_edge(26, GPIO.RISING)
-print("button pressed")
+GPIO.add_event_detect(26, GPIO.RISING, callback=print_f, bouncetime=50)
 socket.run(app,host="192.168.29.7",port="80",debug=True)
 
 
