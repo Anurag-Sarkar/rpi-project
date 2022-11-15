7d0c4b23a7632c0b1365ca2ff4dde8867fc8c131
unknown <sarkaranurag104@gmail.com>
2022-11-15 15:20:33 +0530

    h

--- web.py
+++ web.py
@@ -44,7 +44,7 @@ lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
                                       lcd_d7, lcd_columns, lcd_rows)
 lcd.clear()
 
-lcd.message("hehe")
+lcd.message = "hehe"
 
 
 
