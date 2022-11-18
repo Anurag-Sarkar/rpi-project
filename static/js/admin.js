59204f46143a4babcaf60ae312de378bb4a4b3f7
unknown <sarkaranurag104@gmail.com>
2022-11-18 12:21:09 +0530

    hello\

--- static/js/admin.js
+++ static/js/admin.js
@@ -7,22 +7,22 @@ document.querySelector("#left").addEventListener("click",data=>{
 })
 socket.on("after",function(data){
     console.log(data.data)
-    // document.querySelector("#details h2").textContent = data.name
-    // document.querySelector("#details #avg #avgtime").textContent = data.avg
-    // document.querySelector("#details #avg #tw").textContent = data.totaltime
-    // document.querySelector("#details #holi #th").textContent = data.holidays
-    // document.querySelector("#details #holi #tl").textContent = data.late
-    // clutter = ""
-    // data.attendence.forEach(deta=>{
-    //     stuff = `<div id="dets">
-    //     <p>${deta.date}</p>
-    //                 <p>${deta.entry}</p>
-    //                 <p>${deta.exit}</p>
-    //                 </div>`
-    //     clutter += stuff
-    //     document.querySelector("#last").innerHTML = clutter
+    document.querySelector("#details h2").textContent = data.data.name
+    document.querySelector("#details #avg #avgtime").textContent = data.data.avg
+    document.querySelector("#details #avg #tw").textContent = data.data.totaltime
+    document.querySelector("#details #holi #th").textContent = data.data.holidays
+    document.querySelector("#details #holi #tl").textContent = data.data.late
+    clutter = ""
+    data.data.attendence.forEach(deta=>{
+        stuff = `<div id="dets">
+        <p>${deta.date}</p>
+                    <p>${deta.entry}</p>
+                    <p>${deta.exit}</p>
+                    </div>`
+        clutter += stuff
+        document.querySelector("#last").innerHTML = clutter
 
-    // })
+    })
 
 
 })
