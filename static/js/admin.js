210e8e548adfe40a4d38e36325086966daddc009
unknown <sarkaranurag104@gmail.com>
2022-11-18 12:45:41 +0530

    hello\

--- static/js/admin.js
+++ static/js/admin.js
@@ -4,6 +4,8 @@ const socket = io();
 document.querySelector("#left").addEventListener("click",data=>{
     socket.emit("getdata",nam=data.target.id )
     console.log(data.target.id)
+    document.querySelector("#right").style.display = "inherit"
+    
 })
 socket.on("after",function(data){
     console.log(data.data)
