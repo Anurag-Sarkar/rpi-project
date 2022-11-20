baa79f1475e537d377c6fc7548a79718b9cecfc8
unknown <sarkaranurag104@gmail.com>
2022-11-20 17:42:15 +0530

    hello

--- static/js/profile.js
+++ static/js/profile.js
@@ -9,5 +9,22 @@ document.querySelector("#user").addEventListener("change",data=>{
 console.log("hello")
 
 socket.on("after",function(data){
-    console.log(data.data)
+    document.querySelector("#user").style.display = "none"
+    document.querySelector("#right").style.display = "block"
+    document.querySelector("#details h2").textContent = data.data.name
+    document.querySelector("#details #avg #avgtime").textContent = data.data.avg
+    document.querySelector("#details #avg #tw").textContent = data.data.totaltime
+    document.querySelector("#details #holi #th").textContent = data.data.holiday
+    document.querySelector("#details #holi #tl").textContent = data.data.late
+    clutter = ""
+    data.data.attendence.forEach(deta=>{
+        stuff = `<div id="dets">
+        <p>${deta.date}</p>
+                    <p>${deta.entry}</p>
+                    <p>${deta.exit}</p>
+                    </div>`
+        clutter += stuff
+        document.querySelector("#last").innerHTML = clutter
+
+    })
 })
