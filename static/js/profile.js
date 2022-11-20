13e3ad8c130578efd0b5b44012b4ad5fb5624055
unknown <sarkaranurag104@gmail.com>
2022-11-20 17:39:58 +0530

    hello

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,4 +1,10 @@
 document.querySelector("#user").addEventListener("change",data=>{
     console.log(data.target.value)
+    socket.emit("getdata",nam=data.target.value )
+
 })
 console.log("hello")
+
+socket.on("after",function(data){
+    console.log(data.data)
+})
