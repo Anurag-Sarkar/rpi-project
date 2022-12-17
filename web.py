fa16c7cae266ce586428607201a2ebeb91f414ee
unknown <sarkaranurag104@gmail.com>
2022-12-17 15:57:24 +0530

    init

--- web.py
+++ web.py
@@ -294,6 +294,7 @@ def enter():
 
 
             else:
+                print("user aready in work")
                 check_holiday = attendence.find_one({"name":cu["name"]})
                 if check_holiday["remark"] == "holiday":
                     halfday = cu["halfday"]
